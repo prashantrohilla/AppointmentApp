8da235ed950c5e06fbfabe98133b486a731b9289
Prashant Rohillla <prashantrohilla32@gmail.com>
2021-07-10 19:08:44 +0530

    OptionsActivity

---
+++ app/src/main/java/com/chatapp/example/flamingoapp/OptionsActivity.java
@@ -0,0 +1,16 @@
+package com.chatapp.example.flamingoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.phone.DoctorAppointment.R;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
